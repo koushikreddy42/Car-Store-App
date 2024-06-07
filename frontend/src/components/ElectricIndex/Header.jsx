bc5463ca82defb0326c5b63c4a62cf89e1883d94
koushikreddy42 <koushikreddy877@gmail.com>
2024-06-08 00:18:31 +0530

    added cart button

--- frontend/src/components/ElectricIndex/Header.jsx
+++ frontend/src/components/ElectricIndex/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Header.module.css'
 import logo from '../Assets/t1.png'
 import {Link} from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
 export const Header = () => {
 
   return (
@@ -12,8 +14,17 @@ export const Header = () => {
         <div class={styles.category_div}>
         <Link to="/electric"><button class={styles.category_button}>EVs</button></Link>
         <Link to="/gas"><button class={styles.category_button}>Gas</button></Link>
-        <button class={styles.category_button}>Rent Cars</button>
+        <button class={styles.category_button}>Rent</button>
         </div>
+        <button className={styles.category_button}>
+        <FaShoppingCart className="cart-icon" style={{ color: 'white' }} />
+        <span>Cart</span>
+        </button>
+        <button class={styles.logout_button}>Logout</button>
+        <div style={{ display: 'flex', alignItems: 'center', color: 'white',marginLeft:80 }}>
+      <FaUser style={{ fontSize: '1.2em', marginRight: '10px', color: 'white' }} />
+      <span style={{ fontSize: '1.2em', fontWeight: '500', color: 'white',fontFamily: 'Roboto, sans-serif' }}>Welcome User</span>
+    </div>
     </div>
   )
 }
