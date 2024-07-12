01aeca5ad186ca9cbaad8ef03fe1f82b5bddedd1
vedavalli <pvedavalli27@gmail.com>
2024-07-12 15:24:11 +0530

    home pages ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,49 +1,55 @@
-import React, { useState,useContext,useEffect } from 'react';
-import { store } from '../App';
-import { Navigate,Link } from 'react-router-dom';
-import axios from 'axios';
-import styles from '../components/Home/Home.module.css'
-import videoBg from '../components/Assets/bgvideo.mp4'
-import logo from '../components/Assets/logo.png'
+import React, { useState, useContext, useEffect } from "react";
+import { store } from "../App";
+import { Navigate, Link } from "react-router-dom";
+import axios from "axios";
+import styles from "../components/Home/Home.module.css";
+import videoBg from "../components/Assets/bgvideo.mp4";
+import logo from "../components/Assets/logo.png";
 
-
-function Home(){
-    const [token,setToken]=useContext(store)
-    const [data,setData]=useState(null)
-    if(!token){
-        return <Navigate to='/sign'/>
-    }
-    useEffect(()=>{
-        axios.get('http://localhost:8080/api/myprofile',{
-                headers:{
-                    'x-token':token
-                }
-            }).then(res=>setData(res.data)).catch(err=>console.log(err))
-    },[])
-    return (
-        <div className={styles.homebody}>
-          {data && (
-            <>
-              <video className={styles.video} src={videoBg} autoPlay loop muted />
-              <div className={styles.content}>
-                <div className={styles.navigate}>
-                  <img className={styles.logo} src={logo} alt="logo" />
-                  <div className={styles.support}>
-                    <div className={styles.help}>SERVICES</div>
-                    <div className={styles.help}>ABOUT</div>
-                    <div className={styles.help}>TEAM</div>
-                    <div className={styles.help}>CONTACT</div>
-                  </div>
-                </div>
-                <div className={styles.categories}>
-                <Link to="/electric"><button className={styles.category}>Electric</button></Link>
-                <Link to="/gas"><button className={styles.category}>Gas</button></Link>
-                  <button className={styles.category}>Rent</button>
-                </div>
+function Home() {
+  const [token, setToken] = useContext(store);
+  const [data, setData] = useState(null);
+  if (!token) {
+    return <Navigate to="/sign" />;
+  }
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/myprofile", {
+        headers: {
+          "x-token": token,
+        },
+      })
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className={styles.homebody}>
+      {data && (
+        <>
+          <video className={styles.video} src={videoBg} autoPlay loop muted />
+          <div className={styles.content}>
+            <div className={styles.navigate}>
+              <img className={styles.logo} src={logo} alt="logo" />
+              <div className={styles.support}>
+                <div className={styles.help}>SERVICES</div>
+                <div className={styles.help}>ABOUT</div>
+                <div className={styles.help}>TEAM</div>
+                <div className={styles.help}>CONTACT</div>
               </div>
-            </>
-          )}
-        </div>
-      );
+            </div>
+            <div className={styles.categories}>
+              <Link to="/electric">
+                <button className={styles.category}>Electric</button>
+              </Link>
+              <Link to="/gas">
+                <button className={styles.category}>Gas</button>
+              </Link>
+              <button className={styles.category}>Rent</button>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 export default Home;
