65bce3e18e8e8b22049438304cb11c01c0e6146b
vedavalli <pvedavalli27@gmail.com>
2024-07-12 18:16:42 +0530

    forgot password styling

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import styles from "../components/Login/Login.module.css";
+import styles from "../components/ForgotPassword.module.css";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
@@ -24,19 +24,28 @@ function ForgotPassword() {
 
   return (
     <div className={styles.container}>
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-          required
-        />
-        <button type="submit">Reset Password</button>
-      </form>
-      {message && <p className={styles.success}>{message}</p>}
-      {error && <p className={styles.error}>{error}</p>}
+      <div className={styles.wrapper}>
+        <h2>Forgot Password</h2>
+        <form className={styles.box} onSubmit={handleSubmit}>
+          {!email && (
+            <div className={styles.req}>
+              *Enter the email associated with your account
+            </div>
+          )}
+
+          <input
+            className={styles.email}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+          <button type="submit">Reset Password</button>
+        </form>
+        {message && <p className={styles.success}>{message}</p>}
+        {error && <p className={styles.error}>{error}</p>}
+      </div>
     </div>
   );
 }
